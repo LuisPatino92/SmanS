50a98da7b1cbb4ac807c6aa93217f0de0081ba2c
Lucho <luapatinoar@unal.edu.co>
2023-10-02 20:21:51 -0500

    back - multivalue handled
    - Now backs handles multiple fields data.

--- back/main_handler.py
+++ back/main_handler.py
@@ -1,19 +1,24 @@
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import unquote_plus
 
+import pdb
 
 class CustomHandler(BaseHTTPRequestHandler):
 
     def get_payload(self):
         length = int(self.headers["Content-Length"])
-        raw_content = self.rfile.read(length)
-        clean_content = unquote_plus(raw_content.decode("utf-8"))
-        key, value = clean_content.split("=", 1)
+        raw_content = self.rfile.read(length).decode("utf-8")
+        data = {}
+        for token in raw_content.split("&"):
+            clean_content = unquote_plus(token)
+            key, value = clean_content.split("=", 1)
+            data.update({key: value})
 
-        return {key: value}
+        return data
 
 
     def do_POST(self):
         data = self.get_payload()
+        pdb.set_trace()
 
         #TODO: Implement payload management
